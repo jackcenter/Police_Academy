3e4af0decf3875ad48570dce0555585167007fc7
Jack and Chadd <jace3373@colorado.edu>
2020-03-06 10:55:55 -0700

    fix variable names

--- sensors/sonar_python.py
+++ sensors/sonar_python.py
@@ -9,21 +9,21 @@ ECHO_1 = 19
 
 print("Distance Measurement in Progress")
 
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+GPIO.setup(TRIG_1, GPIO.OUT)
+GPIO.setup(ECHO_1, GPIO.IN)
 
-GPIO.output(TRIG, False)
+GPIO.output(TRIG_1, False)
 print("Waiting for Sensor to Settle")
 time.sleep(2)
 
-GPIO.output(TRIG, True)
+GPIO.output(TRIG_1, True)
 time.sleep(0.00001)
-GPIO.output(TRIG, False)
+GPIO.output(TRIG_1, False)
 
-while GPIO.input(ECHO) == 0:
+while GPIO.input(ECHO_1) == 0:
     pulse_start = time.time()
 
-while GPIO.input(ECHO) == 1:
+while GPIO.input(ECHO_1) == 1:
     pulse_end = time.time()
 
 pulse_duration = pulse_end - pulse_start
