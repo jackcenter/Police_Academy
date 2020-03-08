bc420228554c426f833cc86f2b3a38b07a5ea04d
Jack Center <jace3373@gmail.com>
2020-03-08 16:46:33 -0600

    added error function to be sent to arduino

--- chassis_feedback.py
+++ chassis_feedback.py
@@ -14,6 +14,7 @@ def main():
     trig_pins, echo_pins, units = sonar.setup()
 
     while not exit_program:
+        # TODO: make this just send the error array, inputs are velocities
         r = input('Enter something, "q" to quit"')
         print(r)
 
@@ -22,6 +23,9 @@ def main():
 
     distances = sonar.get_sonar_readings(trig_pins, echo_pins, units)
     print("Measurements: {0}, {1}, {2}".format(distances[0], distances[1], distances[2]))
+    center_error = sonar_to_motor_P_feedback(distances)
+    print(center_error)
+
     GPIO.cleanup()
 
 
@@ -30,6 +34,17 @@ def ConvertStringToBytes(src):
     for b in src:
         converted.append(ord(b))
     return converted
+
+
+def sonar_to_motor_P_feedback(measurements: list):
+    left_reading = measurements[0]
+    right_reading = measurements[1]
+
+    errors = [left_reading - right_reading, right_reading - left_reading]
+    gain = 1
+    correction = [1*x for x in errors]
+
+    return errors
 
 
 if __name__ == '__main__':
