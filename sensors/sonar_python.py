3410a43337da772e55aebffc871c53c7637ed5b3
Jack and Chadd <jace3373@colorado.edu>
2020-03-08 15:17:40 -0600

    added functionality for 3 sensors

--- sensors/sonar_python.py
+++ sensors/sonar_python.py
@@ -3,33 +3,65 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 
-TRIG_1 = 18
-ECHO_1 = 19
+def setup():
+    units = 'in'    # 'cm'
+    TRIG_1 = 18
+    ECHO_1 = 19
+    TRIG_2 = 20
+    ECHO_2 = 21
+    TRIG_3 = 22
+    ECHO_3 = 23
+    
+    TRIG_PINS = [TRIG_1, TRIG_2, TRIG_3]
+    ECHO_PINS = [ECHO_1, ECHO_2, ECHO_3]
+    
+    print("Distance Measurement in Progress")
+    
+    for trig, echo in zip(TRIG_PINS, ECHO_PINS):
+        GPIO.setup(trig, GPIO.OUT)
+        GPIO.setup(echo, GPIO.IN)
+        GPIO.output(trig, False)
+    
+    print("Waiting for Sensor to Settle")
+    time.sleep(2)
+    
+    return TRIG_PINS, ECHO_PINS, units
+
+def main():
+    trig_pins, echo_pins, units = setup()
+
+    for trigger, echo in zip(trig_pins, echo_pins):
+        send_pulse(trigger)
+        pulse_duration = get_pulse_duration(echo)
+        distance = convert_duration_to_distance(pulse_duration, units)
+        print("Distance {0}: {1} {2}".format(trigger, distance, units))
+        time.sleep(0.03)
+    GPIO.cleanup()
 
 
-print("Distance Measurement in Progress")
+def send_pulse(trig: int, duration=0.00001):
+    GPIO.output(trig, True)
+    time.sleep(duration)
+    GPIO.output(trig, False)
+    
 
-GPIO.setup(TRIG_1, GPIO.OUT)
-GPIO.setup(ECHO_1, GPIO.IN)
+def get_pulse_duration(echo: int):
+    while GPIO.input(echo) == 0:
+        pulse_start = time.time()
 
-GPIO.output(TRIG_1, False)
-print("Waiting for Sensor to Settle")
-time.sleep(2)
+    while GPIO.input(echo) == 1:
+        pulse_end = time.time()
 
-GPIO.output(TRIG_1, True)
-time.sleep(0.00001)
-GPIO.output(TRIG_1, False)
+    pulse_duration = pulse_end - pulse_start
+    return pulse_duration
 
-while GPIO.input(ECHO_1) == 0:
-    pulse_start = time.time()
 
-while GPIO.input(ECHO_1) == 1:
-    pulse_end = time.time()
+def convert_duration_to_distance(duration: float, units='in'):
+    unit_dict = {'cm': 17150, 'in': 6750}
+    unit_conversion = unit_dict.get(units)
+    distance = duration * unit_conversion
+    return distance
+    
 
-pulse_duration = pulse_end - pulse_start
-distance = pulse_duration * 17150   # centimeters
-distance = round(distance, 2)
-
-print("Distance: {} cm".format(distance))
-
-GPIO.cleanup()
+if __name__ == '__main__':
+    main()
