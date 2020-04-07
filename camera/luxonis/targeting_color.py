a891fd253787398efe3bbf880fa0996411eb11d8
Chadd Miller <millerchadd21@gmail.com>
2020-04-07 17:26:41 -0600

    added lots of command line options as well as pid and basic i2c additions ... still cannot handle more than one target at a time

--- camera/luxonis/targeting_color.py
+++ camera/luxonis/targeting_color.py
@@ -1,3 +1,14 @@
+'''
+Chadd Miller
+MCEN5115
+CU Boulder
+SP2020
+
+Variation on the luxonis test.py code.  Uses color filtering and calls a PID
+controller from simple_pid to send appropriate commands to the turret stepper
+motors for pitch and rotation.
+'''
+import smbus
 import sys
 from time import time
 from time import sleep
@@ -9,7 +20,7 @@ import cv2
 # from __future__ import print_function
 from difflib import SequenceMatcher
 import depthai
-
+import simple_pid
 import consts.resource_paths
 from depthai_helpers import utils
 
@@ -31,6 +42,13 @@ def limit(inputVal,limits):
         output[i]=val
 
     return output
+
+# i2c comms
+def ConvertStringToBytes(src):
+    converted = []
+    for b in src:
+        converted.append(ord(b))
+    return converted
 ###########################################################################
 
 
@@ -50,8 +68,12 @@ def parse_args():
     python3 test.py -co '{"streams": [{"name": "depth_sipp", "max_fps": 12.0}]}'
     '''
     parser = ArgumentParser(epilog=epilog_text,formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-create_trackbars", default=None, action='store_true', help="Creates trackbars for color adjustment.")
-    parser.add_argument("-imshow_debug", default=None, action='store_true', help="Displays camera windows for viewing and displays print statements for debugging.")
+    parser.add_argument("-to", "--timeout_time", default=None, action='store', help="Sets timeout time.")
+    parser.add_argument("-ct", "--create_trackbars", default=None, action='store_true', help="Creates trackbars for color adjustment.")
+    parser.add_argument("-ppid", "--pitch_pid_modify", default=None, action='append', help="Modifies the pitch PID controller of the form [kp, ki, kd, setpoint] To change, enter -pid kp -pid ki -pid kd -pid x_pixel_setpoint")
+    parser.add_argument("-rpid", "--rotate_pid_modify", default=None, action='append', help="Modifies the rotation PID controller of the form [kp, ki, kd, setpoint] To change, enter -pid kp -pid ki -pid kd -pid y_pixel_setpoint")
+    parser.add_argument("-io", "--i2c_off", default=None, action='store_true', help="Specifying this argument turns off the i2c communication.")
+    parser.add_argument("-imd", "--imshow_debug", default=None, action='store_true', help="Displays camera windows for viewing and displays print statements for debugging.")
     parser.add_argument("-co", "--config_overwrite", default=None,
                         type=str, required=False,
                         help="JSON-formatted pipeline config object. This will be override defaults used in this script.")
@@ -67,9 +89,29 @@ if args['config_overwrite']:
 
 print("Using Arguments=",args)
 
+imshow_debug = False
 if args['imshow_debug']:
     imshow_debug = True
 
+
+if args['timeout_time'] is not None:
+    timeout_time = float(args['timeout_time'])
+else:
+    timeout_time = 150.0
+
+if args['pitch_pid_modify'] is not None:
+    pitch_pid_modifier  = args['pitch_pid_modify']
+    
+if args['rotate_pid_modify'] is not None:
+    rotate_pid_modifier = args['rotate_pid_modify']
+
+if args['i2c_off']:
+    communication_on = False
+else:
+    communication_on = True
+
+
+trackbars_on = False
 if args['create_trackbars']:
     trackbars_on = True
     imshow_debug = True
@@ -106,7 +148,7 @@ config = {
     # To test depth use:
     #'streams': [{'name': 'depth_sipp', "max_fps": 12.0}, {'name': 'previewout', "max_fps": 12.0}, ],
     #'streams': [{'name': 'previewout', "max_fps": 3.0}, {'name': 'depth_mm_h', "max_fps": 3.0}],
-    'streams': [{'name': 'previewout', "max_fps": 5.0}, {'name': 'metaout', "max_fps": 5.0}],
+    'streams': [{'name': 'previewout', "max_fps": 9.0}, {'name': 'metaout', "max_fps": 5.0}],
     #'streams': ['metaout', 'previewout'],
     'depth':
     {
@@ -150,6 +192,7 @@ if p is None:
 
 
 t_start = time()
+time_start = time()
 frame_count = {}
 frame_count_prev = {}
 for s in stream_names:
@@ -157,6 +200,18 @@ for s in stream_names:
     frame_count_prev[s] = 0
 
 entries_prev = []
+
+
+##################    I2C COMMUNICATION SETUP    ####################
+if communication_on:
+    bus = smbus.SMBus(1)
+
+# I2C address of Arduino Slave
+slave_address = 0x08
+# I think this is the register we're trying to read out of/into?
+i2c_cmd = 0x01
+
+
 
 
 ################## ADDED FOR COLOR DETECTION CWM ####################
@@ -255,7 +310,53 @@ pause=0
 ####################################################################
 #Setting up the targeting program:
 
-reference = (150,150)
+
+yref = 150
+p_kp = 0.1
+p_ki = 0
+p_kd = 0.01
+
+xref = 150
+r_kp = 0.1
+r_ki = 0
+r_kd = 0.01
+
+if pitch_pid_modifier is not None:
+    ppid_args_length = len(pitch_pid_modifier)
+    if ppid_args_length is 4:
+        try:
+            p_kp                  = float(pitch_pid_modifier[0])
+            p_ki                  = float(pitch_pid_modifier[1])
+            p_kd                  = float(pitch_pid_modifier[2])
+            yref                  = float(pitch_pid_modifier[3])
+        except:
+            print("PITCH ARGUMENT ERROR -- INCORRECT PITCH PID ARGUMENTS SYNTAX -- RETURNING TO DEFAULT VALUES")
+            yref = 150
+            p_kp = 0.1
+            p_ki = 0
+            p_kd = 0.01        
+    else:
+        print("ERROR -- INCORRECT LENGTH OF PITCH PID ARGUMENTS, LENGTH SHOULD BE 4, BUT LENGTH WAS: " + str(ppid_args_length))
+
+
+if rotate_pid_modifier is not None:
+    rpid_args_length = len(rotate_pid_modifier)
+    if rpid_args_length is 4:
+        try:
+            r_kp                  = float(rotate_pid_modifier[0])
+            r_ki                  = float(rotate_pid_modifier[1])
+            r_kd                  = float(rotate_pid_modifier[2])
+            xref                  = float(rotate_pid_modifier[3])
+        except:
+            print("ROTATE ARGUMENT ERROR -- INCORRECT ROTATE PID ARGUMENTS SYNTAX -- RETURNING TO DEFAULT VALUES")
+            xref = 150
+            r_kp = 0.1
+            r_ki = 0
+            r_kd = 0.01        
+    else:
+        print("ERROR -- INCORRECT LENGTH OF ROTATE PID ARGUMENTS, LENGTH SHOULD BE 4, BUT LENGTH WAS: " + str(rpid_args_length))
+
+
 
 ####################################################################
 
@@ -519,11 +620,6 @@ while True:
                                 print('z:' '{:7.3f}'.format(e[0]['distance_z']))
             
             
-            # Do stuff here with targeting, simple PID reference - bad_guy_center or whatever
-            
-            # Then send commands to arduino over wire or USB 
-            
-            
             if imshow_debug:
                 print('----------frame over ----------')
 
@@ -568,7 +664,45 @@ while True:
                     cv2.imshow(packet.stream_name, frame)
         
         frame_count[packet.stream_name] += 1
-
+    
+    
+    
+    
+    # This is the end of the "while loop" -- pid and command sending will likely work here.    
+#################### PID AND COMMUNICATIONS #################################################
+            
+    # compute new ouput from the PID according to the systems current value
+#            control = pid(v)
+    bad_guy_x = bad_guy_center[0]
+    bad_guy_y = bad_guy_center[1]
+    
+    pitch_controller    = simple_pid.PID(p_kp, p_ki, p_kd, setpoint=yref)
+    pitch_command       = round(pitch_controller(bad_guy_y), 2)
+    p_cmd               = "{:.2f}".format(pitch_command)
+    
+    rotate_controller   = simple_pid.PID(r_kp, r_ki, r_kd, setpoint=xref)
+    rotate_command      = round(rotate_controller(bad_guy_x), 2)
+    r_cmd               = "{:.2f}".format(rotate_command)
+    
+    # if commands go to within some range, send command to fire
+    if pitch_command < 0.1 and rotate_command < 0.1:
+        p_cmd = "f"
+        r_cmd = "f"
+    
+    # Then send commands to arduino over i2c wire or USB 
+    if communication_on:
+        bytesToSend = ConvertStringToBytes(r_cmd + ',' + p_cmd)
+        print(bytesToSend)
+        bus.write_i2c_block_data(slave_address, i2c_cmd, bytesToSend)
+    
+    
+    
+    
+#########################################################################################    
+    
+    
+    
+    
     t_curr = time()
     if t_start + 1.0 < t_curr:
         t_start = t_curr
@@ -578,6 +712,9 @@ while True:
             frame_count[s] = 0
 
     if cv2.waitKey(1) == ord('q'):
+        break
+    
+    if t_curr - time_start > timeout_time:
         break
 
 del p  # in order to stop the pipeline object should be deleted, otherwise device will continue working. This is required if you are going to add code after the main loop, otherwise you can ommit it.
