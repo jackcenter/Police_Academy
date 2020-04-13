4fa4fb5086d47deedba7aec37420d1da03347704
Jack Center <jace3373@gmail.com>
2020-04-13 15:10:42 -0600

    added pos and vel updates

--- base/encoders.py
+++ base/encoders.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from math import pi
 import smbus
 import time
 
@@ -21,11 +22,27 @@ def run_test():
     print("Left Encoder Reading:  {}".format(left_value))
     print("Right Encoder Reading: {}".format(right_value))
 
+    leftEncoder = Encoder('left', 100, bus, slave_address)
+    rightEncoder = Encoder('right', 100, bus, slave_address)
+    driveTrain = DriveTrain(leftEncoder, rightEncoder, bus, slave_address)
+
+    leftEncoder.update()
+    leftEncoder.print_value()
+
+    rightEncoder.update()
+    rightEncoder.print_value()
+
+    driveTrain.update()
+    leftEncoder.print_value()
+    rightEncoder.print_value()
+
 
 class Encoder:
-    def __init__(self, location, serial_address):
+    def __init__(self, location, cpr, bus, slave_address):
         self.location = location
-        self.serial_address = serial_address
+        self.cpr = cpr
+        self.bus = bus
+        self.slave_address = slave_address
 
         self.position = 0.0
         self.previous_position = 0.0
@@ -33,12 +50,21 @@ class Encoder:
         self.k0 = 0.0
         self.k1 = 0.0
 
-    def update(self):
+        self.parent = None
+
+    def update(self, value=None):
         self.k0 = self.k1
         self.k1 = time.time()
 
         self.previous_position = self.position
-        self.position = get_encoder_values(self.location, self.serial_address)
+
+        if value:
+            self.position = value
+
+        else:
+            self.position = get_encoder_values(self.bus, self.slave_address, self.location)
+
+        self.compute_velocity()
 
     def get_value(self):
         return self.position
@@ -52,22 +78,51 @@ class Encoder:
     def get_velocity(self):
         return self.velocity
 
+    def set_parent(self, parent):
+        self.parent = parent
 
-def get_encoder_values(bus, slave_address):
+    def print_value(self):
+        print("Position: ".format(self.position))
+        print("Velocity: ".format(self.veloicty))
+
+    def compute_velocity(self):
+        e_dot = (self.position - self.previous_position)/(self.k1 - self.k0)
+        self.velocity = 2*pi*e_dot/self.cpr
+
+
+class DriveTrain:
+    def __init__(self, enc_left, enc_right, bus, slave_address):
+        self.enc_left = enc_left
+        self.enc_right = enc_right
+        self.bus = bus
+        self.slave_address = slave_address
+
+        enc_left.set_parent(self)
+        enc_right.set_parent(self)
+
+    def update(self):
+        left_value, right_value = get_encoder_values(self.bus, self.slave_address)
+        self.enc_left.update(left_value)
+        self.enc_right.update(right_value)
+
+
+def get_encoder_values(bus, slave_address, location=None):
     data_bytes = bus.read_i2c_block_data(slave_address, 0, 8)
     data_int_r = bytes_to_int(data_bytes[0:3])
     data_int_l = bytes_to_int(data_bytes[4:7])
 
-    return data_int_l, data_int_r
+    if location == 'left':
+        return data_int_l
+
+    elif location == 'right':
+        return data_int_r
+
+    else:
+        return data_int_l, data_int_r
 
 
 def bytes_to_int(data):
-    result = 0
-    # bytes.reverse()
     return int.from_bytes(data, byteorder='little', signed=True)
-    # for b in bytes:
-    #     result = result * 256 + int(b)
-    # return result
 
 
 if __name__ == "__main__":
