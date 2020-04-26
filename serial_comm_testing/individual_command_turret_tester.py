438d3e3110af74a0183f0b77ccc3ff54233b931d
Chadd Miller <millerchadd21@gmail.com>
2020-04-25 21:45:00 -0600

    changed byte order from big to little, little is definitely correct, something else is going on.  Added homing back into individual commands tester file

--- serial_comm_testing/individual_command_turret_tester.py
+++ serial_comm_testing/individual_command_turret_tester.py
@@ -15,8 +15,8 @@ from turret_commands import send_home_cmd
 from turret_commands import send_fire_cmd
 
 # TODO turn pitch steps into pitch degrees
-#trash = input("Homing on your command!")
-#send_home_cmd()
+trash = input("Homing on your command!")
+send_home_cmd()
 #trash = input("Firing on your command!")
 #send_fire_cmd()
 
